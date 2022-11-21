42cdff3f57d7358cb6d5028b039698116f464ce5
Diego França <diego.franca92@gmail.com>
2022-11-21 14:08:09 -0300

    yup category

--- src/yup/services.ts
+++ src/yup/services.ts
@@ -3,3 +3,7 @@ import * as yup from 'yup';
 export const yupCreateServiceInput = yup.object().shape({
   name: yup.string().required(),
 });
+
+export const yupCreateCategoryInput = yup.object().shape({
+  name: yup.string().required(),
+});
